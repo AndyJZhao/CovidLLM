3e49b460328d5b5c7a180cad6ca284ae526be211
andy-border <zja00999@gmail.com>
2023-10-30 12:23:24 +0800

    [v3.0 Data Update] + 3.1 Shorter Prompt + 3.2 Static Data

--- src/utils/data/prompt_tree.py
+++ src/utils/data/prompt_tree.py
@@ -7,23 +7,23 @@ from utils.pkg.dict2xml import dict2xml
 import re
 from collections import OrderedDict
 
+
 class PromptTree:
     def __init__(self, cfg, data, id, hierarchy, name_alias, style='xml', label=None):
-        self.info_dict = info_dict = OrderedDict({
-            'Static': data[id][cfg.data.static_cols].T.squeeze().to_dict(),
-            'Dynamic': data[id][cfg.data.dynamic_cols].T.squeeze().to_dict(),
-        })
+        static_info = data[id].Static_description if cfg.use_static_text \
+            else data[id][cfg.data.static_cols].T.squeeze().to_dict()
+        info_dict = data[id][cfg.data.dynamic_cols].T.squeeze().to_dict()
         if cfg.use_seq_encoder:
             for cont_field in cfg.in_cont_fields:
-                self.info_dict['Dynamic'][cont_field] = f'<{cont_field.upper()}-EMB>'
+                info_dict[cont_field] = f'<{cont_field.upper()}-EMB>'
         self.style = style
         self.hierarchy = hierarchy
         self.label = label
-        prompt = ''
+        prompt = static_info + '\n'
         if self.style == 'json':
-            prompt = json.dumps(info_dict, indent=4)
-        elif self.style == 'xml' or self.style == 'xml_wo_text':
-            prompt = dict2xml(info_dict, wrap="information", indent="\t")
+            prompt += json.dumps(info_dict, indent=4)
+        elif self.style == 'xml':
+            prompt += dict2xml(info_dict, wrap="information", indent="\t")
         self.prompt = prompt
 
     def __str__(self):
