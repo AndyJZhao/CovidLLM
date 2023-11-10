a71fd5e76a98d87c5053d8eb3a1d023135751572
andy-border <zja00999@gmail.com>
2023-11-10 08:19:21 +0800

    [v4.1 PredictionDistribution] Finished

--- src/covid_llm/agent.py
+++ src/covid_llm/agent.py
@@ -14,7 +14,7 @@ from omegaconf import OmegaConf
 from utils.basics import init_path, lot_to_tol, time_logger
 from utils.pkg.distributed import master_process_only
 from .model import IGNORE_INDEX
-from .metrics import calc_acc, calc_mse_from_cls_labels
+from .metrics import calc_acc, calc_mse_from_cls_labels, calc_prediction_distribution
 
 logging.getLogger("transformers").setLevel(logging.WARNING)
 logging.getLogger("transformers.tokenization_utils").setLevel(logging.ERROR)
@@ -71,7 +71,7 @@ class Agent:
                 results[f'{split}_acc'] = calc_acc(label, pred)
             if 'mse' in self.cfg.metrics:
                 results[f'{split}_mse'] = calc_mse_from_cls_labels(label, pred, self.data.mse_val_map)
-
+            results.update({f'{split}-PD/{k}': v for k, v in calc_prediction_distribution(pred, self.model.cls_token_names)})
         logger.warning(results)
         return results
 
--- src/covid_llm/metrics.py
+++ src/covid_llm/metrics.py
@@ -1,6 +1,7 @@
 import numpy as np
 from sklearn.metrics import mean_squared_error
 
+
 def calc_acc(label, pred):
     return np.mean(label == pred) if len(label) == len(pred) else -1
 
@@ -9,3 +10,13 @@ def calc_mse_from_cls_labels(label, pred, val_map):
     val_map_func = np.vectorize(dict(val_map).get)
     y_true, y_pred = val_map_func(label), val_map_func(pred)
     return mean_squared_error(y_true, y_pred)
+
+
+def calc_prediction_distribution(pred, class_names):
+    unique, counts = np.unique(pred, return_counts=True)
+    res = {val: cnt / len(pred)
+           for val, cnt in zip(unique, counts)}
+    # Set non-predicted labels to 0 frequency
+    for cls in class_names:
+        res[cls] = res.get(cls, 0)
+    return res
